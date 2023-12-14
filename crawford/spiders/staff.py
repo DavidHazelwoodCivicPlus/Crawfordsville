6ce94c42f2291704e7c7b594e9305fafd9ee9429
david.hazelwood <david.hazelwood@civicplus.com>
2023-12-14 16:33:27 -0600

    Finished creating the scrapping script and the files it output. Total time for completion was 1 to 1.5 hours.

--- crawford/spiders/staff.py
+++ crawford/spiders/staff.py
@@ -4,7 +4,24 @@ import scrapy
 class StaffSpider(scrapy.Spider):
     name = "staff"
     allowed_domains = ["www.crawfordsville.net"]
-    start_urls = ["https://www.crawfordsville.net/"]
+    start_urls = ["https://www.crawfordsville.net/egov/apps/staff/directory.egov?eGov_searchName=&eGov_searchDepartment=&app=6&sect=public&path=browse&act=&id=&page=6_3&order="]
 
     def parse(self, response):
-        pass
+        rows = response.xpath('//tbody/tr')
+        for row in rows :
+            name = row.xpath('td/a/text()').extract_first()
+            title = row.xpath("td[@class='eGov_DataCell2']/text()").get()
+            department = row.xpath("td[@class='eGov_DataCell3']/text()").get()
+            email = row.xpath("td[@class='eGov_DataCell4']/a").get()
+            phone = row.xpath("td[@class='eGov_DataCell5']/text()").get()
+          
+
+
+            # Everything in the yield section does not need to be touched.
+            yield {
+                'Name' : name,
+                'Title' : title,
+                'Department' : department,
+                'Email' : email,
+                'Phone' : phone,
+            }
